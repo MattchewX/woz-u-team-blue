6b806a651dc0e96c31b801729bb79c19f9826ae5
erikwilliams <33586131+erikwilliams@users.noreply.github.com>
2020-07-12 20:49:09 -0700

    Basic Home page added
    Rough draft home page added

--- client-react/src/components/Home.js
+++ client-react/src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import '../task.min.css'
+import './home.css'
 class Home extends React.Component {
   constructor (props) {
    super(props);
@@ -8,7 +9,36 @@ class Home extends React.Component {
   componentDidMount() {}
   render() {
    return (
-<div>Home Page In Progress</div>
+//<div id= "test">Home Page In Progress</div>
+
+<wrapper>
+      <div class="header">
+
+    
+    <div class="row">
+  <div class="col-4">
+    <ul>
+      Piper's Pizza
+    </ul>
+    </div>
+
+     <img class="col-4"
+              src="https://files.slack.com/files-pri/T0ECY9XMK-F01602XBNFQ/scottish-bagpiper-piper-in-national-clothes-vector-13914275.jpg"/>
+
+
+<div class="col-4">
+    <ul>
+      (1-800-PIPERSPIZZA)
+    </ul>          
+  </div>
+  </div>
+</div>
+</wrapper>
+
+
+    
+
+
    );
   }
  }
